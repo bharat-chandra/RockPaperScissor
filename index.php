06e62200d9e3c51a557de7e13abf3644e1277791
bharat-chandra <53616992+bharat-chandra@users.noreply.github.com>
2020-12-11 00:03:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCKTYPE html>
+<html>
+    <head>
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width , initial-scale=1">
+    </head>
+    <body style="background:graytext">
+        <div class="container">
+            <div class="row">
+               <div>
+                   <h3 class="display-1" style="color: white"><center>Rock Paper Scissor</center></h3>
+               </div><br>
+                <div class="col-xs-4"  >
+                    <img src="rock.png" class="img-responsive" style="width:250px;height: 250px">
+                </div>
+                <div class="col-xs-4">
+                   <img src="paper.png" class="img-responsive" style="width:230px;height: 250px"" >
+                </div>
+                <div class="col-xs-4">
+                   <img src="scissor.png" class="img-responsive" style="width:200px;height: 250px"" >
+                </div>
+            </div>  
+        </div>
+        <div class="container" style="margin-top: 100px;background: blue">
+            <center>
+            <div>
+                <a href="start.html"><button class="btn btn-lg btn-default">start the game</button></a>
+            </div>
+            </center>
+        </div>
+        <script type="text/javascript">
+            
+        </script>
+    </body>
+</html>
+
+
+
